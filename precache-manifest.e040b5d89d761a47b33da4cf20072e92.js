b0c79c3ecbc7bf3c78c5beb9f376d3aa6b9d3ed7
DushaSmile <hover234@gmail.com>
2019-01-18 01:16:44 +0300

    Updates

---
+++ precache-manifest.e040b5d89d761a47b33da4cf20072e92.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "db51892f75868c6a7f2d",
+    "url": "/fintech/static/js/runtime~main.db51892f.js"
+  },
+  {
+    "revision": "2ba57bb31e281a80e44f",
+    "url": "/fintech/static/js/main.2ba57bb3.chunk.js"
+  },
+  {
+    "revision": "db59ca17e0c62ada9019",
+    "url": "/fintech/static/js/1.db59ca17.chunk.js"
+  },
+  {
+    "revision": "13cb9f2a4e49f4ea859f8a9bc67a8263",
+    "url": "/fintech/index.html"
+  }
+];
